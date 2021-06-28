6f87e5eb132f9db9591ce9320ce184fa10569825
anwar sanusi 12 <55305792+Anwarsan9412@users.noreply.github.com>
2021-06-28 20:20:30 +0700

    Delete urls.py

--- home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import HomeView, CategoryView,Search
-
-urlpatterns = [
-    path('',HomeView.as_view(), name='home'),
-    path('category/<pk>/', CategoryView, name='category'),
-    path('search/', Search, name='search'),
-    
-]
